aca959438a8e8430390896230ad2c419ec42ae74
Rodolfo Ruiz <rpruiz@me.com>
2020-12-02 18:18:27 -0600

    connecting to ipfs and getting their README

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use ipfs::{Ipfs, IpfsOptions, IpfsPath, TestTypes, UninitializedIpfs};
+use std::process::exit;
+use tokio::{io::AsyncWriteExt, stream::StreamExt};
+
+#[tokio::main]
+async fn main() {
+    // Initialize an in-memory repo and start a daemon
+    let opts = IpfsOptions::inmemory_with_generated_keys();
+    let (ipfs, fut): (Ipfs<TestTypes>, _) = UninitializedIpfs::new(opts).start().await.unwrap();
+
+    // Spawn the background task
+    tokio::task::spawn(fut);
+
+    // Restore the default bootstrappers to enable content delivery
+    ipfs.restore_bootstrappers().await.unwrap();
+
+    // Get the IPFS README
+    let path = "/ipfs/QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc/readme"
+        .parse::<IpfsPath>()
+        .unwrap();
+
+    let stream = ipfs.cat_unixfs(path, None).await.unwrap_or_else(|e| {
+        eprintln!("Error: {}", e);
+        exit(1);
+    });
+
+    tokio::pin!(stream);
+
+    let mut stdout = tokio::io::stdout();
+
+    loop {
+        match stream.next().await {
+            Some(Ok(bytes)) => {
+                stdout.write_all(&bytes).await.unwrap();
+            }
+            Some(Err(e)) => {
+                eprintln!("Error: {}", e);
+                exit(1);
+            }
+            None => break,
+        }
+    }
 }
